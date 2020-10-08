df490415c82ffced5b14ba9fd0ca5ae8156204a7
Nicolas Barbosa <n.barbosa.pro@gmail.com>
2020-10-08 14:04:38 +0200

    Initial Commit

---
+++ App-BDE-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App-BDE-iOS
+//
+//  Created by Nicolas Barbosa on 08/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ App-BDE-iOSTests/App_BDE_iOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  App_BDE_iOSTests.swift
+//  App-BDE-iOSTests
+//
+//  Created by Nicolas Barbosa on 08/10/2020.
+//
+
+import XCTest
+@testable import App_BDE_iOS
+
+class App_BDE_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
