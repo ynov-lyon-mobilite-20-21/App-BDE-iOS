741463aefb68f25ddcc5290a4d05cdc4699d008d
Nicolas Barbosa <nicolas.barbosa@orange.com>
2020-11-07 15:23:45 +0100

    Updated to start authentication implementation

--- App-BDE-iOS/Screens/Profil/ProfilView.swift
+++ App-BDE-iOS/Screens/Profil/ProfilView.swift
@@ -9,61 +9,81 @@ import SwiftUI
 
 struct ProfilView: View {
     @Environment(\.colorScheme) var colorScheme
-
+    @State private var isConnected = false
+    @State private var showModal: Bool = false
+    
+    
     var body: some View {
+        
+        
+        
         GeometryReader { gr in
-            VStack {
-                HStack {
-                    ZStack {
-                        VStack {
+            ScrollView {
+                VStack {
+                    HStack {
+                        ZStack {
+                            VStack {
+                                Spacer()
+                                TitleCustom(textColor: .white, shadowColor: .bdeGreen, size: 35, title: "MON COMPTE")
+                            }
+                            .frame(minWidth: 0, maxWidth: gr.size.width - 35, minHeight: gr.size.height * 0.2, maxHeight: gr.size.height * 0.2)
+                            .background(Color.blueToBlack)
+                            .clipShape(ProfilTopShape())
                             Spacer()
-                            TitleCustom(textColor: .white, shadowColor: .bdeGreen, size: 35, title: "MON COMPTE")
                         }
-                        .frame(minWidth: 0, maxWidth: gr.size.width - 35, minHeight: gr.size.height * 0.2, maxHeight: gr.size.height * 0.2)
-                        .background(Color.blueToBlack)
-                        .clipShape(ProfilTopShape())
+                        Button("test") {
+                            self.isConnected = true
+                        }
+                        .alert(isPresented: $isConnected) {
+                            Alert(title: Text("Vous n'êtes pas connecté"), primaryButton: Alert.Button.default(Text("Se connecter")){
+                                self.showModal.toggle()
+                            }
+                            
+                            , secondaryButton: .cancel())
+                        }
+                        .sheet(isPresented: self.$showModal) { ContactsView() }
+                        
+                        Spacer()
+                    }
+                    HStack {
+                        Image("hugo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .clipShape(Circle())
+                            .overlay(ZStack{
+                                Circle().stroke(Color.bdeGreen, lineWidth: 2)
+                                    .offset(x: 4, y: 1)
+                                Circle().stroke(Color.pinkToYellow, lineWidth: 2)
+                                    .offset(x: -6, y: -6)
+                            })
+                        VStack{
+                            TitleCustom(textColor: .blackToWhite, shadowColor: .greenToYellow, size: 25, title: "Nicolas Barbosa")
+                            Text("\nM1 Expert Dev WEB")
+                                .font(.custom("FashionFetish", size: 12))
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                .foregroundColor(.blackToWhite)
+                                .padding(.leading)
+                                .offset(x: 0, y: -20)
+                        }
+                    }
+                    .frame(minHeight: 0, maxHeight: gr.size.height * 0.2)
+                    .padding(.leading, 30)
+                    HStack {
+                        UserInfoView(textColor: .blackToWhite, shadowColor: .pinkToGreen, size: 22, userAdress: "nicolas@test.com", userPhoneNumber: "07 86 35 62 13", userInscription: "Adhérent", lineWidth: gr.size.width * 0.8)
                         Spacer()
                     }
                     Spacer()
                 }
-                HStack {
-                    Image("hugo")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .clipShape(Circle())
-                        .overlay(ZStack{
-                            Circle().stroke(Color.bdeGreen, lineWidth: 2)
-                                .offset(x: 4, y: 1)
-                            Circle().stroke(Color.pinkToYellow, lineWidth: 2)
-                                .offset(x: -6, y: -6)
-                        })
-                    VStack{
-                        TitleCustom(textColor: .blackToWhite, shadowColor: .greenToYellow, size: 25, title: "Nicolas Barbosa")
-                        Text("\nM1 Expert Dev WEB")
-                            .font(.custom("FashionFetish", size: 12))
-                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                            .foregroundColor(.blackToWhite)
-                            .padding(.leading)
-                            .offset(x: 0, y: -20)
-                    }
-                }
-                .frame(minHeight: 0, maxHeight: gr.size.height * 0.2)
-                .padding(.leading, 30)
-                HStack {
-                    UserInfoView(textColor: .blackToWhite, shadowColor: .pinkToGreen, size: 22, userAdress: "nicolas@test.com", userPhoneNumber: "07 86 35 62 13", userInscription: "Adhérent", lineWidth: gr.size.width * 0.8)
-                    Spacer()
-                }
-                Spacer()
             }
         }
         .background(
             ZStack {
-            Color.whiteToBlue
-            Image("background_event")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .opacity(colorScheme == .dark ? 0.2 : 1)
-        })
+                Color.whiteToBlue
+                Image("background_event")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .opacity(colorScheme == .dark ? 0.2 : 1)
+            })
         .edgesIgnoringSafeArea(.top)
     }
 }
@@ -73,8 +93,8 @@ struct ProfilView_Previews: PreviewProvider {
         Group {
             ProfilView()
                 .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
-                
-                
+            
+            
             ProfilView()
                 .previewDevice("iPhone 8")
         }
