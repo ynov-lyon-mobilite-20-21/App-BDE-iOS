5b14d93495b402c579ec99128ca3a2d103623f9a
Nicolas Barbosa <nicolas.barbosa@orange.com>
2021-03-18 17:33:00 +0100

    Added netfox

--- App-BDE-iOS/Application/EntryPoint.swift
+++ App-BDE-iOS/Application/EntryPoint.swift
@@ -6,10 +6,12 @@
 //
 
 import SwiftUI
+import Combine
 
 @main
 struct EntryPoint: App {
-    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ErrorHandler {
