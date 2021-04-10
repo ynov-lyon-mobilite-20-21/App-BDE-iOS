0b913f017dff46f7ef992c96622414dc632e23d7
Nicolas Barbosa <nicolas.barbosa@orange.com>
2021-04-10 14:01:50 +0200

    Update UI

--- App-BDE-iOS/Epics/Profile/ProfileView.swift
+++ App-BDE-iOS/Epics/Profile/ProfileView.swift
@@ -67,9 +67,9 @@ struct ProfileView: View {
                         // User info block
                         HStack {
                             // User Info
-                            VStack(alignment: .leading, spacing: 10) {
+                            VStack(alignment: .leading, spacing: 15) {
                                 HStack {
-                                    TitleCustom(title: "\(viewModel.user?.firstName ?? "") \(viewModel.user?.lastName ?? "")",
+                                    TitleCustom(title: "\(viewModel.user?.firstName ?? "") \(viewModel.user?.lastName.uppercased() ?? "")",
                                                 font: Font.title3.weight(.bold),
                                                 textColor: Color.blackToWhite,
                                                 shadowColor: Color.bdeGreen)
@@ -91,8 +91,8 @@ struct ProfileView: View {
                                 Text("\(viewModel.user?.promotion.rawValue ?? "") \(viewModel.user?.formation.rawValue ?? "")")
                                 Text(viewModel.user?.mail ?? "")
                             }
-                            .padding(.leading)
-                            .padding(.vertical, 25)
+                            .padding()
+                            
 
                             Spacer()
                             VStack {
