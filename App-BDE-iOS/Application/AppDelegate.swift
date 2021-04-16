96d57ae035efb1ed66f1aa1c189d4f6a2b6cbd29
Nicolas Barbosa <nicolas.barbosa@orange.com>
2021-04-16 22:48:48 +0200

    Update appDelegate

--- App-BDE-iOS/Application/AppDelegate.swift
+++ App-BDE-iOS/Application/AppDelegate.swift
@@ -11,45 +11,10 @@ import Stripe
 import NetFox
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil,
-                     restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         #if DEBUG
-        NFX.sharedInstance().start()
+            NFX.sharedInstance().start()
         #endif
-        
-        switch userActivity.activityType {
-                case NSUserActivityTypeBrowsingWeb:
-                guard let url = userActivity.webpageURL else { return false }
-                    print("Universal link url : \(url)")
-                // display the correct screen corresponding to the url
-                return true
-                default: return false
-            }
-        
-//        // Get URL components from the incoming user activity.
-//        guard userActivity.activityType == NSUserActivityTypeBrowsingWeb,
-//              let incomingURL = userActivity.webpageURL,
-//              let components = NSURLComponents(url: incomingURL, resolvingAgainstBaseURL: true) else {
-//            return false
-//        }
-//
-//        // Check for specific URL components that you need.
-//        guard let path = components.path,
-//              let params = components.queryItems else {
-//            return false
-//        }
-//        print("path = \(path)")
-//
-//        if let paymentReturn = params.first(where: { $0.name == "stripe/paymentReturn" } )?.value {
-//
-//            print("paymentReturn = \(paymentReturn)")
-//            return true
-//
-//        } else {
-//            print("Missing stripe payment return")
-//            return false
-//        }
         return true
     }
 }
